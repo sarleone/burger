0889b16d8235d0a7bd56383c26650628cefb09e8
sarleone <sarah.m.leone@gmail.com>
2018-03-11 22:14:56 -0500

    setting up backend and front end connections

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Black Bean and Roasted Pepper Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Avocado and Queso Fresco', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cucubmer and Gouda', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('California Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Bacon Burger');
