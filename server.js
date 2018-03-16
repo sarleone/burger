5d9d453ee919732a4e00a9669a9e979b06f3e0f3
sarleone <sarah.m.leone@gmail.com>
2018-03-16 00:31:02 -0500

    heroku export changes

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var app = express();
 
 var PORT = process.env.PORT || 8888;
 
-var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
