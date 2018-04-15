37aa77e845caf6381ab07d31627d4a54a1bd3196
sarleone <sarah.m.leone@gmail.com>
2018-04-14 23:56:54 -0500

    working Burger

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 
-var PORT = process.env.PORT || 8888;
+var PORT = process.env.PORT || 3000;
 
 
 // Serve static content for the app from the "public" directory in the application directory.
